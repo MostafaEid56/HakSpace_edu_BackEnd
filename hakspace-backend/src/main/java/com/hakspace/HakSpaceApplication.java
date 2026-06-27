4377c7af9734fbb7d57de8466a9e96384777341c
Mostafa Eid <sosta12345sosta@gmail.com>
2026-06-27 12:04:26 +0300

    Delete hakspace-backend directory

--- hakspace-backend/src/main/java/com/hakspace/HakSpaceApplication.java
+++
@@ -1,8 +0,0 @@
-package com.hakspace;
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-@SpringBootApplication
-public class HakSpaceApplication {
-    public static void main(String[] args) { SpringApplication.run(HakSpaceApplication.class, args); }
-}
--- hakspace-backend/src/main/java/com/hakspace/repository/CourseRepository.java
+++
@@ -1,5 +0,0 @@
-package com.hakspace.repository;
-import com.hakspace.model.Course;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CourseRepository extends JpaRepository<Course, Long> {}
